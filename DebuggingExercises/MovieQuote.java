e5d1c99e06b76d0b2fbbe0d0e05cdba03a8229d2
art-smb2 <sharyn.barr@my.jcu.edu.au>
2017-08-07 14:31:41 +1000

    Hello

---
+++ DebuggingExercises/MovieQuote.java
@@ -0,0 +1,10 @@
+/**
+ * Created by art-smb2 on 7/08/17.
+ */
+public class MovieQuote {
+    public static void main (String[] args)
+{
+    System.out.println("Go Ahead - Make My Day!");
+}
+}
+
---
+++ DebuggingExercises/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by art-smb2 on 7/08/17.
+ */
+public class MovieQuoteInfo
+{
+    public static void main (String[] args)
+{
+    System.out.println("Go Ahead - Make My Day!");
+    System.out.println("From the movie Sudden Impact released in 1983");
+    System.out.println("Stated by the character of Harry Callahan played by Clint Eastwood");
+}
+}
